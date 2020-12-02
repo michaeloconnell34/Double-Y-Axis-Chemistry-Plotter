a5985576c4f04b39c8eb9a2d8753edccdc8329b8
michaeloconnell34 <72401964+michaeloconnell34@users.noreply.github.com>
2020-12-02 05:35:24 -0700

    Add files via upload

---
+++ Python Bicarbonate - Copy - Copy.py
@@ -0,0 +1,173 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import openpyxl
+from pathlib import Path
+import gc
+
+data = pd.read_csv('FHr.csv', usecols = ['STATION','Electrical Conductivity', 'Total Dissolved Solids' , 'Chloride', 'Sulphate','Sodium', 'Bicarbonate', 'Carbonate', 'Fluoride', 'Bromide', 'Ion Balance', 'DATE', 'Water', 'Ground Level'])
+print(data.head())
+print(data)
+
+
+
+grouped = data.groupby('STATION')
+
+
+
+### Bicarbonate
+
+
+
+def Bicarbonate():
+   grouped = data.groupby('STATION')
+   for name,group in grouped:
+      print (name)
+      print (group)
+
+      fig, ax = plt.subplots() # Create the figure and axes object
+
+      # Plot the first x and y axes:
+      group.plot(x = 'DATE', y = 'Bicarbonate', ax = ax, title = name) 
+      # Plot the second x and y axes. By secondary_y = True a second y-axis is requested:
+      # (see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.html for details)
+      group.plot(x = 'DATE', y = 'Water', ax = ax, secondary_y = True)
+      group.plot(x = 'DATE', y = 'Ground Level', ax= ax, secondary_y = True)
+      plt.savefig(name + ' Bicarbonate', dpi=500, bbox_inches='tight')
+
+
+def Electrical_Conductivity():
+   for name,group in grouped:
+      print (name)
+      print (group)
+
+      fig, ax = plt.subplots() # Create the figure and axes object
+
+      # Plot the first x and y axes:
+      group.plot(x = 'DATE', y = 'Electrical Conductivity', ax = ax, title = name) 
+      # Plot the second x and y axes. By secondary_y = True a second y-axis is requested:
+      # (see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.html for details)
+      group.plot(x = 'DATE', y = 'Water', ax = ax, secondary_y = True)
+      group.plot(x = 'DATE', y = 'Ground Level', ax= ax, secondary_y = True)
+      plt.savefig(name + ' Electrical Conductivity', dpi=500, bbox_inches='tight')
+
+def Chloride():
+   for name,group in grouped:
+      print (name)
+      print (group)
+
+      fig, ax = plt.subplots() # Create the figure and axes object
+
+      # Plot the first x and y axes:
+      group.plot(x = 'DATE', y = 'Chloride', ax = ax, title = name) 
+      # Plot the second x and y axes. By secondary_y = True a second y-axis is requested:
+      # (see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.html for details)
+      group.plot(x = 'DATE', y = 'Water', ax = ax, secondary_y = True)
+      group.plot(x = 'DATE', y = 'Ground Level', ax= ax, secondary_y = True)
+      plt.savefig(name + ' Chloride', dpi=500, bbox_inches='tight')
+
+
+def Sulphate():
+   for name,group in grouped:
+      print (name)
+      print (group)
+
+      fig, ax = plt.subplots() # Create the figure and axes object
+
+      # Plot the first x and y axes:
+      group.plot(x = 'DATE', y = 'Sulphate', ax = ax, title = name) 
+      # Plot the second x and y axes. By secondary_y = True a second y-axis is requested:
+      # (see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.html for details)
+      group.plot(x = 'DATE', y = 'Water', ax = ax, secondary_y = True)
+      group.plot(x = 'DATE', y = 'Ground Level', ax= ax, secondary_y = True)
+      plt.savefig(name + ' Sulphate', dpi=500, bbox_inches='tight')
+
+
+def Sodium():
+   for name,group in grouped:
+      print (name)
+      print (group)
+
+      fig, ax = plt.subplots() # Create the figure and axes object
+
+      # Plot the first x and y axes:
+      group.plot(x = 'DATE', y = Sodium, ax = ax, title = name) 
+      # Plot the second x and y axes. By secondary_y = True a second y-axis is requested:
+      # (see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.html for details)
+      group.plot(x = 'DATE', y = 'Water', ax = ax, secondary_y = True)
+      group.plot(x = 'DATE', y = 'Ground Level', ax= ax, secondary_y = True)
+      plt.savefig(name + ' Sodium', dpi=500, bbox_inches='tight')
+
+def Carbonate():
+   for name,group in grouped:
+      print (name)
+      print (group)
+
+      fig, ax = plt.subplots() # Create the figure and axes object
+
+      # Plot the first x and y axes:
+      group.plot(x = 'DATE', y = 'Carbonate', ax = ax, title = name) 
+      # Plot the second x and y axes. By secondary_y = True a second y-axis is requested:
+      # (see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.html for details)
+      group.plot(x = 'DATE', y = 'Water', ax = ax, secondary_y = True)
+      group.plot(x = 'DATE', y = 'Ground Level', ax= ax, secondary_y = True)
+      plt.savefig(name + ' Carbonate', dpi=500, bbox_inches='tight')
+
+def Fluoride():
+   for name,group in grouped:
+      print (name)
+      print (group)
+
+      fig, ax = plt.subplots() # Create the figure and axes object
+
+      # Plot the first x and y axes:
+      group.plot(x = 'DATE', y = 'Fluoride', ax = ax, title = name) 
+      # Plot the second x and y axes. By secondary_y = True a second y-axis is requested:
+      # (see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.html for details)
+      group.plot(x = 'DATE', y = 'Water', ax = ax, secondary_y = True)
+      group.plot(x = 'DATE', y = 'Ground Level', ax= ax, secondary_y = True)
+      plt.savefig(name + ' Fluoride', dpi=500, bbox_inches='tight')
+
+def Bromide():
+   for name,group in grouped:
+      print (name)
+      print (group)
+
+      fig, ax = plt.subplots() # Create the figure and axes object
+
+      # Plot the first x and y axes:
+      group.plot(x = 'DATE', y = 'Bromide', ax = ax, title = name) 
+      # Plot the second x and y axes. By secondary_y = True a second y-axis is requested:
+      # (see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.html for details)
+      group.plot(x = 'DATE', y = 'Water', ax = ax, secondary_y = True)
+      group.plot(x = 'DATE', y = 'Ground Level', ax= ax, secondary_y = True)
+      plt.savefig(name + ' Bromide', dpi=500, bbox_inches='tight')
+
+def Ion_Balance():
+   for name,group in grouped:
+      print (name)
+      print (group)
+
+      fig, ax = plt.subplots() # Create the figure and axes object
+
+      # Plot the first x and y axes:
+      group.plot(x = 'DATE', y = 'Ion Balance', ax = ax, title = name) 
+      # Plot the second x and y axes. By secondary_y = True a second y-axis is requested:
+      # (see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.html for details)
+      group.plot(x = 'DATE', y = 'Water', ax = ax, secondary_y = True)
+      group.plot(x = 'DATE', y = 'Ground Level', ax= ax, secondary_y = True)
+      plt.savefig(name + ' Ion Balance', dpi=500, bbox_inches='tight')
+
+
+
+Bicarbonate()
+Electrical_Conductivity()
+Chloride()
+Sulphate()
+Sodium()
+Carbonate()
+Fluoride()
+Bromide()
+Ion_Balance()
+
+      
